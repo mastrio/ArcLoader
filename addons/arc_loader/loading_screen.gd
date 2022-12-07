a5450a877d5d4c013d9f7f4efd38fd8d9ffd69d2
MASTRIO <mastrio5772@gmail.com>
2022-12-07 11:43:45 +1000

    now it actually loads stuff

--- addons/arc_loader/loading_screen.gd
+++ addons/arc_loader/loading_screen.gd
@@ -1,28 +1,51 @@
 extends Panel
 
 
+var current_scene: Node = null
+var new_scene: String = ""
+
 var load_checker = preload("res://addons/arc_loader/load_checker.tscn")
 var current_load_checker: Node2D
 
+var fade_in = false
 
-func _ready():
-	await get_tree().create_timer(1).timeout
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _physics_process(_delta):
+	get_tree().root.move_child(self, get_tree().root.get_node("/root").get_child_count())
+
+func start_loading():
+	if not is_instance_valid(current_scene):
+		ArcLoader.log_err("Error loading scene, current_scene is not a valid scene")
+		return
 	
-	var new_scene = ArcLoader._scene_to_load.instantiate()
-	while not is_instance_valid(new_scene):
-		new_scene = ArcLoader._scene_to_load.instantiate()
+	await get_tree().create_timer(1).timeout
+	var loading_scene = load(new_scene).instantiate()
+	
+	if not is_instance_valid(loading_scene):
+		ArcLoader.log_err("Error loading scene, new_scene is not a valid scene")
+		return
 	
 	current_load_checker = load_checker.instantiate()
 	
-	new_scene.add_child(current_load_checker)
-	current_load_checker.connect("scene_loaded", scene_ready)
-	get_tree().root.add_child(new_scene)
+	loading_scene.add_child(current_load_checker)
+	current_load_checker.connect("scene_loaded", _scene_ready)
+	get_tree().root.add_child(loading_scene)
+	
+	new_scene = ""
+	current_scene = null
 
-func scene_ready():
-	$AnimationPlayer.stop(true)
-	$AnimationPlayer.play("FadeIn")
+func _scene_ready():
+	animation_player.stop(true)
+	animation_player.play("Fade")
 
 func _on_animation_player_animation_finished(anim_name):
-	print("AH")
-	if anim_name == "FadeIn":
-		queue_free()
+	if anim_name == "Fade":
+		if not fade_in:
+			current_scene.queue_free()
+			start_loading()
+			animation_player.stop(true)
+			animation_player.play("SPIN")
+		
+		fade_in = not fade_in
--- addons/arc_loader/plugin.gd
+++ addons/arc_loader/plugin.gd
@@ -4,7 +4,9 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_autoload_singleton("ArcLoader", "res://addons/arc_loader/arc_loader.gd")
+	add_autoload_singleton("LoadingScreen", "res://addons/arc_loader/loading_screen.tscn")
 
 
 func _exit_tree():
 	remove_autoload_singleton("ArcLoader")
+	remove_autoload_singleton("LoadingScreen")
